5b31b9960f1a5aa5664de3eca066e33c3e46abcf
ReyheNegub <ReyheNegub@outlook.com>
2021-03-07 20:36:07 -0300

    Level 04

---
+++ Assets/Scripts/Movimentar.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimentar : MonoBehaviour
+{
+    public Vector2 direcao;
+
+    public float velocidade;
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    private void Update()
+    {
+        transform.Translate(direcao * velocidade * Time.deltaTime);
+    }
+}
