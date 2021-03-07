2517a2657e5f1c12d6bddb2e2122101ca43f9514
ReyheNegub <ReyheNegub@outlook.com>
2021-03-07 19:35:37 -0300

    Level 02

---
+++ Assets/Scripts/Jogador.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float forcaPulo = 700;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Pular();
+        }
+    }
+
+    void Pular()
+    {
+        rb.AddForce(Vector2.up * forcaPulo);
+    }
+}
