b20be4aae03078df499feec55ffb935f6db905a2
ReyheNegub <ReyheNegub@outlook.com>
2021-03-08 19:58:06 -0300

    Level 14

---
+++ Assets/Scripts/GeradorInimigos.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeradorInimigos : MonoBehaviour
+{
+
+    public GameObject[] cactoPrefabs;
+
+    public GameObject dinossauroVoadorPrefab;
+    public float dealyInicial;
+    public float delayEntreCactos;
+
+    public float dinossauroVoadorYMinimo = -1;
+    public float dinossauroVoadorYMaximo = 1;
+    // Start is called before the first frame update
+    public float dinossauroVoadorPontuacaoMinima = 300;
+
+    public Jogador jogadorScript;
+    private void Start()
+    {
+        InvokeRepeating("GerarInimigo", dealyInicial, delayEntreCactos);
+    }
+
+    // Update is called once per frame
+    private void GerarInimigo()
+    {
+        var dado = Random.Range(1, 7);
+        if (jogadorScript.pontos >= dinossauroVoadorPontuacaoMinima && dado <= 2)
+        {
+            var posicaoYAleatoria = Random.Range(dinossauroVoadorYMinimo, dinossauroVoadorYMaximo);
+            var posicao = new Vector3(
+                transform.position.x,
+                transform.position.y + posicaoYAleatoria,
+                transform.position.z
+            );
+
+            Instantiate(dinossauroVoadorPrefab, posicao, Quaternion.identity);
+        }
+        else
+        {
+            var quantidadeCactos = cactoPrefabs.Length;
+            var indiceAleatorio = Random.Range(0, quantidadeCactos);
+            var cactoPrefab = cactoPrefabs[indiceAleatorio];
+            Instantiate(cactoPrefab, transform.position, Quaternion.identity);
+        }
+    }
+}
