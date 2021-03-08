31ac291ce988664a9a492cfda2b8296ce646a73e
ReyheNegub <ReyheNegub@outlook.com>
2021-03-07 22:20:45 -0300

    Level 05

---
+++ Assets/Scripts/GeradorCacto.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeradorCacto : MonoBehaviour
+{
+
+    public GameObject cactoPrefab;
+    public float dealyInicial;
+    public float delayEntreCactos;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        InvokeRepeating("GerarCacto", dealyInicial, delayEntreCactos);
+    }
+
+    // Update is called once per frame
+    private void GerarCacto()
+    {
+        Instantiate(cactoPrefab, transform.position, Quaternion.identity);
+    }
+}
